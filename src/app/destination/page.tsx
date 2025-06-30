12ac9905caa55dfc78c11653cc9c07736544768d
Siddhant Vats <siddhantvats840@gmail.com>
2025-06-30 13:04:26 +0530

    commit

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import Image from "next/image";
-import Hero from "./components/Hero"
+import Hero from "./components/Hero";
 import Country from "./components/Country";
+import Groups from "../../../components/Groups";
 
 export default function Home() {
   return (
     <>
       <Hero></Hero>
-     <Country/>
+      <Country />
+      <Groups></Groups>
     </>
   );
 }
